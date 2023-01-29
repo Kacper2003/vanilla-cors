bad59bc2ec440068a727e5b832018ec2c3bb78aa
Kacper2003 <lapper.kaczynski@gmail.com>
2023-01-29 13:55:36 +0000

    API call 10

--- app/page.jsx
+++ app/page.jsx
@@ -6,8 +6,8 @@ export default function Home() {
             const res = await fetch(
                 'https://157.97.1.155:9004/v1/login',
                 {
-                    method: 'POST',
-                    headers: {
+                    'method': 'POST',
+                    'headers': {
                         'Content-Type': 'application/json',
                         'Accept': 'application/json',
                         'Authorization': 'Basic',
@@ -16,7 +16,7 @@ export default function Home() {
                         'Access-Control-Request-Headers': 'origin',
                         'Access-Control-Request-Method': 'POST',
                     },
-                    body: {
+                    'body': {
                         'userid': 'Kacper',
                         'password': '1234',
                         'usertype': 1

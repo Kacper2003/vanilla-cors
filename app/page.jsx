f5f9fa70f9dd5b0f0ac351c1430acbdcb9a5e026
Kacper2003 <lapper.kaczynski@gmail.com>
2023-01-29 14:16:27 +0000

    API call 16

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,7 @@ export default function Home() {
     const callAPI = async () => {
         try {
             const res = await fetch(
-                'https://157.97.1.155:9004/v1/login',
+                'http://157.97.1.155:9004/v1/login',
                 {
                     method: 'POST',
                     headers: {

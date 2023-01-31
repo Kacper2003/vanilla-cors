0a5629c85d07642ac65577d2ab39e00647152067
einsiful <bomdie2002@gmail.com>
2023-01-31 22:27:51 +0000

    API call 35

--- app/page.jsx
+++ app/page.jsx
@@ -6,7 +6,7 @@ export default function Home() {
             const res = await fetch(
                 'https://157.97.1.155:9004/v1/api',
                 {
-                    method: 'POST',
+                    method: 'GET',
                     mode: 'cors',
                     headers: {
                         'origin': 'https://vanilla-cors.vercel.app'
